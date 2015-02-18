3f32291dc856c5f6294ec5bdfaf126da533fc7fb
Ryan Sadio <ryan@sadio.ca>
2015-02-18 12:50:03 -0800

    Config, Caboose, and jRating

--- application/config/config.php
+++ application/config/config.php
@@ -37,7 +37,7 @@ $config['base_url'] = '';
   | variable so that it is blank.
   |
  */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
   |--------------------------------------------------------------------------
@@ -102,7 +102,7 @@ $config['charset'] = 'UTF-8';
   | setting this variable to TRUE (boolean).  See the user guide for details.
   |
  */
-$config['enable_hooks'] = FALSE;
+$config['enable_hooks'] = TRUE;
 
 
 /*
