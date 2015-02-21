00f93969390e0a617d54177da893f456ca37bebe
Ryan Sadio <ryan@sadio.ca>
2015-02-21 14:25:43 -0800

    Show error with style

---
+++ application/views/quote_edit.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryansadio
+ * Date: 15-02-20
+ * Time: 21:14
+ */
+?>
+<div class="row">
+    <div class="errors">{message}</div>
+    <form action="/admin/confirm" method="post">
+        {fid}
+        {fwho}
+        {fmug}
+        {fwhat}
+        {fsubmit}
+    </form>
+</div>
